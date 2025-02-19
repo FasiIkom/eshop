6cdede8c9f51878b1d346312e8df66ca28a963e6
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2025-02-20 04:35:16 +0700

    Fix last commit

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTest.java
@@ -2,9 +2,7 @@ package id.ac.ui.cs.advprog.eshop;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EshopApplicationTest {
 
     @Test
