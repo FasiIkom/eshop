e3d9469ff1e102a293d4569d46ef6a3e1ce2d967
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2025-02-20 04:45:59 +0700

    fix main class test error

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -5,12 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EshopApplication {
-
-    // Private constructor to prevent instantiation
-    private EshopApplication() {
-        throw new UnsupportedOperationException("Utility class");
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(EshopApplication.class, args);
     }
