64a842b0ffa4235f326acfcfa57590a13338e284
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2025-02-20 03:17:34 +0700

    Fix error

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
@@ -1,11 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import id.ac.ui.cs.advprog.eshop.service.ProductService;
 
 @Controller
 @RequestMapping("/")
