e31959cb9cd6809a65473f5e5181a7917d934945
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2025-02-20 04:18:42 +0700

    Fix method name

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomePageController {
 
     @GetMapping("/")
-    public String Homepage() {
+    public String homePage() {
         return "HomePage";
     }
 }
