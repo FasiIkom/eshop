1e9b9a16269f873565d810e958bc328c85b138ae
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2025-03-07 19:48:52 +0700

    [GREEN] Implement PaymentService class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -13,21 +14,33 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment createPayment(Payment payment) {
+        if (paymentRepository.findById(payment.getId()) == null) {
+            paymentRepository.save(payment);
+            return payment;
+        }
         return null;
     }
 
     @Override
     public Payment updateStatus(String paymentId, String status) {
-        return null;
+        Payment payment = paymentRepository.findById(paymentId);
+        if (payment != null) {
+            Payment newPayment = new Payment(payment.getId(), payment.getOrder(), 
+                payment.getMethod(), payment.getPaymentData(),  status);
+            paymentRepository.save(newPayment);
+            return newPayment;
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
     @Override
     public List<Payment> findAllByMethod(String method) {
-        return null;
+        return paymentRepository.findAllByMethod(method);
     }
 
     @Override
     public Payment findById(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 }
