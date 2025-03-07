0b9f08cfb11221bba940c02a9f1d5cc2836a820d
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2025-03-07 09:59:59 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Map;
+import java.util.Arrays;
 
 @Builder
 @Getter
@@ -17,7 +18,23 @@ public class Payment {
     String status;
 
     public Payment(String id, Order order, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = "PENDING";
+        this.paymentData = paymentData;
+        if (order == null) {
+            throw new IllegalArgumentException();
+        } else {
+            this.order = order;
+        }
     }
     public Payment(String id, Order order, String method, Map<String, String> paymentData, String status) {
+        this(id, order, method, paymentData);
+        String[] statusList = {"PENDING", "SUCCESS", "REJECTED"};
+        if(Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
