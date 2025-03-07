c5800a14c821a778a46cadeff2f0f38d261a006f
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2025-03-07 10:44:46 +0700

    [GREEN] Implement PaymentRepository class

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -11,12 +11,34 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
-        return null;
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+
+        paymentData.add(payment);
+        return payment;
     }
     public Payment findById(String id) {
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
     public List<Payment> findAllByMethod(String orderId) {
-        return null;
+        List<Payment> result = new ArrayList<>();
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getMethod().equals(orderId)) {
+                result.add(savedPayment);
+            }
+        }
+        return result;
     }
 }
